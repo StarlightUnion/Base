30810351f790c086b7145f1b7ab3c534992cac2f
StarlightUnion <43316114+StarlightUnion@users.noreply.github.com>
2019-07-25 22:53:02 +0800

    2019年07月25日 22:52:57

--- MVVM/js/observer.js
+++ MVVM/js/observer.js
@@ -9,15 +9,36 @@ function observe (data) {
 }
 
 function defineMVVM (data, key, val) {
+  var subs = new subscribe();
   observe(val);
   Object.defineProperty(data, key, {
     enumerable: true,
     configurable: false,
     get: function () {
+      subscribe.target && subs.add(subscribe.target);
       return val;
     },
     set: function (newVal) {
+      console.log('变化',val,'->',newVal);
       val = newVal;
+      subs.notify();
     }
   });
 }
+
+function subscribe () {
+  this.subs = [];
+}
+
+subscribe.prototype = {
+  add: function (item) {
+    this.subs.push(item);
+  },
+  notify: function() {
+    this.subs.forEach(function (item) {
+      item.update();
+    })
+  }
+}
+
+subscribe.target = null;
