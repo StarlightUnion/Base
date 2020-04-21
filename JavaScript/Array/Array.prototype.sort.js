2b52a32b691f97ff8241e7052fcdbddadea80ebf
StarlightUnion <43316114+StarlightUnion@users.noreply.github.com>
2020-04-21 23:00:02 +0800

    2020年04月21日 22:59:54

---
+++ JavaScript/Array/Array.prototype.sort.js
@@ -0,0 +1,11 @@
+// Created by wxc on 2020/04/21
+
+const months = ['March', 'Jan', 'Feb', 'Dec'];
+console.log(months.sort());// [ 'Dec', 'Feb', 'Jan', 'March' ]
+
+const txts = ['ability', 'absent', 'April', 'divide', 'center', 'context', 'container'];
+console.log(txts.sort());// ['April', 'ability', 'absent', 'center', 'container', 'context', 'divide']
+
+const nums = [1, 12, 10000, 23, 59, 81, 111, 999];
+console.log(nums.sort());// [1, 10000, 111, 12, 23, 59, 81, 999]
+
